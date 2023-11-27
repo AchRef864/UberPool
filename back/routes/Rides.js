6521b678e38a8b843f5521fb13d39482421253bc
AchRef <rahaliashraf732@gmail.com>
2023-11-27 12:21:33 +0100

    hahahaha

--- back/routes/Rides.js
+++ back/routes/Rides.js
@@ -5,7 +5,16 @@ const { createItem, readAll, readItem, updateItem, deleteItem } = require("../to
 
 //Create
 router.post('/', async (req, res) => {
-
+    const ride = new Ride({
+        driver_id: req.body.driver_id,
+        start_location: req.body.start_location,
+        end_location: req.body.end_location,
+        departure_time: req.body.departure_time,
+        arrival_time: req.body.arrival_time,
+        available_seats: req.body.available_seats,
+        ride_description: req.body.ride_description
+    })
+    await createItem(req, res, ride)
 })
 
 //Read all
@@ -21,8 +30,37 @@ router.get('/:id', getRide, async (req, res) => {
 
 //Update
 router.patch('/:id', getRide, async (req, res) => {
+    if (req.body.driver_id != null) {
+        res.ride.driver_id = req.body.driver_id;
+    }
 
-})
+    if (req.body.start_location != null) {
+        res.ride.start_location = req.body.start_location;
+    }
+
+    if (req.body.end_location != null) {
+        res.ride.end_location = req.body.end_location;
+    }
+
+    if (req.body.departure_time != null) {
+        res.ride.departure_time = req.body.departure_time;
+    }
+
+    if (req.body.arrival_time != null) {
+        res.ride.arrival_time = req.body.arrival_time;
+    }
+
+    if (req.body.available_seats != null) {
+        res.ride.available_seats = req.body.available_seats;
+    }
+
+    if (req.body.ride_description != null) {
+        res.ride.ride_description = req.body.ride_description;
+    }
+
+    await updateItem(req, res, res.ride);
+});
+
 
 //Delete
 router.delete('/:id', getRide, async (req, res) => {
